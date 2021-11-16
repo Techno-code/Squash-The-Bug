a8dbd9a3ec22295fd234864315e7ec2e6c9edd2c
Techno-code <66633479+Techno-code@users.noreply.github.com>
2021-11-16 18:15:24 -0500

    finished with the Goal Class

--- MainSquashTheBug.py
+++ MainSquashTheBug.py
@@ -32,14 +32,7 @@ def render_text(window, text, point, font_size, font_colour):
 
     window.blit(text, text_rect)
 
-
-
-
-
-
 # A point class
-
-
 
 class Point():
     def __init__(self, x, y):
@@ -47,7 +40,7 @@ class Point():
         self.y = y
 
 class Goal():
-    def __init__(self, x, y, height, width, border_color, font_colour, text):
+    def __init__(self, x, y, height, width, border_color, font_colour, font_size, text):
         self.x = x
         self.y = y
         self.height = height
@@ -55,11 +48,49 @@ class Goal():
         self.border_color = border_color
         self.text = text
         self.font_colour = font_colour
-        self.font = "arial"
-        self.font_size = 15
+        self.font_size = font_size
 
     def render_goal(self, window):
-        pygame.draw.rect(window, self.border_color, (self.x, self.y, self.width, self.height), 5)
+        rect = pygame.draw.rect(window, self.border_color, (self.x, self.y, self.width, self.height), 5)
+        WrapText(window, self.text, self.font_colour, rect, pygame.font.SysFont("arial", self.font_size))
+
+def WrapText(surface, text, color, rect, font, aa=False, bkg=None):
+    rect = pygame.Rect(rect)
+    y = rect.top + 5
+    lineSpacing = -2
+
+    # get the height of the font
+    fontHeight = font.size("Tg")[1]
+
+    while text:
+        i = 1
+
+        # determine if the row of text will be outside our area
+        if y + fontHeight > rect.bottom:
+            break
+
+        # determine maximum width of line
+        while font.size(text[:i])[0] < (rect.width) and i < len(text):
+            i += 1
+
+        # if we've wrapped the text, then adjust the wrap to the last word      
+        if i < len(text): 
+            i = text.rfind(" ", 0, i) + 1
+
+        # render the line and blit it to the surface
+        if bkg:
+            image = font.render(text[:i], 1, color, bkg)
+            image.set_colorkey(bkg)
+        else:
+            image = font.render(text[:i], aa, color)
+
+        surface.blit(image, (rect.left+8, y))
+        y += fontHeight + lineSpacing
+
+        # remove the text we just blitted
+        text = text[i:]
+
+    return text
 
 
 # Button Class
@@ -89,7 +120,12 @@ window = pygame.display.set_mode((800, 950))
 clock = pygame.time.Clock()
 background_color = (31, 31, 36)
 border_color = (156, 141, 140)
-border_coords = (10, 820, 780, 120)
+text_color = (255,255,0)
+borderx = 10 
+bordery = 820
+width = 780
+height = 120
+test_goal = Goal(borderx, bordery, height, width, border_color, text_color, 25, "Hello this is a test please test work thank you")
 run = True
 
 # Main Loop
@@ -105,9 +141,7 @@ while run:
 
     # Level 1 test
 
-    pygame.draw.rect(window, border_color, border_coords, 5) # width border = 5
-
-    render_text(window, "test", Point(700, 525), 30, (0,0,0))
+    test_goal.render_goal(window)
 
     # Update the screen display
     pygame.display.flip()
